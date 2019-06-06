59f36f48b5d847072cde7ae2ccd66d558b9215ca
Tyrel Chambers <tychambers3@gmail.com>
2019-06-06 18:34:24 -0400

    finish backend

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const Email = require('email-templates');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const port = 3001;
+const email = new Email({
+  message: {
+    from: 'Story Submission <no-replay@southerncannibal.com>'
+  },
+  // uncomment below to send emails in development/test env:
+  send: false,
+  transport: {
+    jsonTransport: true
+  }
+});
+
+app.post('/submit', (req, res) => {
+  const body = req.body;
+
+  email
+  .send({
+    template: 'mars',
+    message: {
+      to: 'tychambers3@gmail.com'
+    },
+    locals: {
+      name: body.name,
+      email: body.email,
+      text: body.story,
+      shared: body.shared
+    }
+  })
+  .then(console.log)
+  .catch(console.error);
+
+  res.status(200).json({message: "Your story has been sent, thank you!"});
+});
+
+app.listen(port, () => console.log("App running on " + port));
