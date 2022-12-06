5ee16a193b0c41fdd1f48c95b1fedcb41fdd994a
Tyrel Chambers <tychambers3@gmail.com>
2022-12-06 15:05:02 -0500

    update emails

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
-require('dotenv').config()
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+require("dotenv").config();
 
-const Email = require('email-templates');
- 
+const Email = require("email-templates");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,41 +14,45 @@ app.use(cors());
 const port = process.env.PORT || 3001;
 const email = new Email({
   message: {
-    from: 'SouthernCannibal.com <no-replay@southerncannibal.com>'
+    from: "SouthernCannibal.com <no-replay@southerncannibal.com>",
   },
   // uncomment below to send emails in development/test env:
   send: true,
   transport: nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-           user: process.env.GMAIL_ACCOUNT,
-           pass: process.env.GMAIL_PASSWORD
-       }
-   })
+      user: process.env.GMAIL_ACCOUNT,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  }),
 });
 
-app.post('/submit', (req, res) => {
-  const body = req.body;
-  email
-  .send({
-    template: 'mars',
-    message: {
-      to: 'southerncannibalstories@gmail.com'
-    },
-    locals: {
-      name: body.name,
-      email: body.email,
-      text: body.story,
-      subject: body.subject,
-      shared: body.shared
-    }
-  })
-  .then()
-  .catch(console.error);
+app.post("/submit", (req, res) => {
+  try {
+    const body = req.body;
+    email
+      .send({
+        template: "mars",
+        message: {
+          to: "southerncannibalstories@gmail.com",
+        },
+        locals: {
+          name: body.name,
+          email: body.email,
+          text: body.story,
+          subject: body.subject,
+          shared: body.shared,
+        },
+      })
+      .then()
+      .catch(console.error);
 
-  res.status(200).json({message: "Your story has been sent, thank you!"});
+    res.status(200).json({ message: "Your story has been sent, thank you!" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => console.log("App running on " + port));
